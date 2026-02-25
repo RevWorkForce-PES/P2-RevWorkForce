ffffc17aad05767cacbfc7083230fca8de679d20
Shal@750 <nigarshahla14@gmail.com>
2026-02-25 15:43:49 +0530

    Dynamic Port Configuration

--- src/main/java/com/revature/revworkforce/RevWorkForceApplication.java
+++ src/main/java/com/revature/revworkforce/RevWorkForceApplication.java
@@ -1,5 +1,8 @@
 package com.revature.revworkforce;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RevWorkForceApplication {
 
     public static void main(String[] args) {
+    	HashMap serverConf=new HashMap();
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the port #");
+		int port=scan.nextInt();
+		serverConf.put("server.port", port);
+		serverConf.put("spring.application.name", "ErrorHanding");
+		serverConf.put("Spring.application.profile", "Development");
+		
+		//SpringApplication.run(ErrorHandingApplication.class, args);
+		SpringApplication sprapp=new SpringApplication(RevWorkForceApplication.class);
+		sprapp.setDefaultProperties(serverConf);
+		sprapp.run(args);
+		
         SpringApplication.run(RevWorkForceApplication.class, args);
         System.out.println("RevWorkforce Application Started Successfully!");
     }
